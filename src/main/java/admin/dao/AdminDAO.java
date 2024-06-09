86233c760a9ebc55d969424c6a07aa8725d5ffad
Sion-Noh <vmfpsel@gmail.com>
2024-06-09 21:10:02 +0900

    admin 메인 대시보드(남여 성비), 로그인, 로그아웃 기능만 구현

--- src/main/java/admin/dao/AdminDAO.java
+++ src/main/java/admin/dao/AdminDAO.java
@@ -1,5 +1,81 @@
 package admin.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class AdminDAO {
+	private static AdminDAO instance;
+	
+	public static AdminDAO  getInstance() {
+		if(instance == null) instance = new AdminDAO();
+		return instance;
+	}
+	
+	private AdminDAO() {}
+	
+	private Connection getConnection() throws Exception {
+		Context ctx = new InitialContext();
+		DataSource db = (DataSource) ctx.lookup("java:comp/env/jdbc/oracle");
+		return db.getConnection();
+	}
+	
+	
+	/**
+	 * 총 회원수와 남자 회원의 회원수를 반환하는 메서드
+	 * @return
+	 * @throws Exception
+	 */
+	public int[] membersTotalCount() throws Exception {
+		String sql = "select count(*), (select count(*) from members where gender = 'M') from members";
+		try(Connection con = getConnection();
+			PreparedStatement pstat = con.prepareStatement(sql);
+			ResultSet rs = pstat.executeQuery()){
+			
+			rs.next();
+			
+			System.out.println("총 회원 수 ======= " + rs.getInt(1));
+			System.out.println("총 남자회원 수 ======= " + rs.getInt(2));
+			
+			return new int[]{rs.getInt(1), rs.getInt(2)};
+			
+		}
+	}
+	
+	
+	/**
+	 * 관리자 로그인
+	 * @param id
+	 * @param pw
+	 * @return
+	 * @throws Exception
+	 */
+	public int adminLogin(String id, String pw) throws Exception {
+		String sql = "select member_id, member_pw from members where id = ? and grade = 99";
+		try(Connection con = getConnection();
+				PreparedStatement pstat = con.prepareStatement(sql);
+				ResultSet rs = pstat.executeQuery()){
+				
+				rs.next();
+				
+				if(rs.getString("id") != null) {
+					if(pw.equals(rs.getString("pw"))) {
+						// 비밀번호까지 맞는 경우
+						return 1;
+					} else {
+						// id는 맞고 비밀번호가 틀린 경우
+					}
+				} else {
+					// 존재하지 않는 아이디
+				}
+				
+			}
+		
+		return 0;
+	}
 	
 }
--- src/main/java/controllers/AdminController.java
+++ src/main/java/controllers/AdminController.java
@@ -8,6 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+import admin.dao.AdminDAO;
+import commons.EncryptionUitls;
 
 
 @WebServlet("*.admin")
@@ -19,27 +23,61 @@ public class AdminController extends HttpServlet {
 		
 		String cmd = request.getRequestURI();
 		
+		AdminDAO dao = AdminDAO.getInstance();
+		
 		// Admin login 여부 확인
-		boolean adminSession = (boolean) request.getSession().getAttribute("WolfAdmin");
+		boolean adminSession = false;
+		if(request.getSession().getAttribute("WolfAdmin") != null) {
+			adminSession = (boolean)request.getSession().getAttribute("WolfAdmin");
+		}
 		
 		try {
 			
 			/** 메인 **/
-			if(cmd.equals("/admin")) {
+			if(cmd.equals("/main.admin")) {
 				System.out.println("Admin page start");
 				
-				// 로그인된 세션이 없다면
-				if(adminSession) response.sendRedirect("admin_login.jsp");
+				// 로그인된 세션이 없다면 로그인 페이지로 강제 이동
+				if(adminSession) response.sendRedirect("/views/admin/admin_login.jsp");
 				else {
 					// 메인 페이지 대시 보드 기능 작성
+					// 1. 남여 비율 대시보드
+					int[] genderCount = dao.membersTotalCount();
+					System.out.println("남성 유저 수 ===== " + genderCount[1]);
+					System.out.println("여성 유저 수 ===== " + (genderCount[0] - genderCount[1]));
+					
+					request.setAttribute("male", genderCount[1]);
+					request.setAttribute("female", (genderCount[0] - genderCount[1]));
+					request.getRequestDispatcher("admin.jsp").forward(request, response);
+					
 				}
 			}
 			
 			/** 로그인 **/
-			else if(cmd.equals("test1")) {
+			else if(cmd.equals("/login.admin")) {
+				System.out.println("Admin login start");
 				
-				int result = 1;	// dao.adminLogin 성공 시
-				if(result == 1) request.getSession().setAttribute("WolfAdmin", true);
+				String admin_id = request.getParameter("admin_id");
+				String admin_pw = request.getParameter("admin_pw");
+				
+				System.out.println("관리자 ID ===== " + admin_id);
+				
+				int result = dao.adminLogin(admin_id, admin_pw);
+				if(result == 1) {
+					request.getSession().setAttribute("WolfAdmin", true);
+					response.sendRedirect("/main.admin");
+				} else {
+					// 로그인 실패
+				}
+				
+			}
+			
+			/** 로그아웃 **/
+			else if(cmd.equals("/logout.admin")) {
+				
+				// 세션 종료
+				request.getSession().invalidate();
+				response.sendRedirect("/views/admin/admin_login.jsp");
 				 
 			}
 			
