f0b75dd9fcf6b60090290b3969dfb321371a6f34
ol7mi <dmsal9865@gmail.com>
2024-06-11 00:14:15 +0900

    index.jsp 효과

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,40 +1,41 @@
 
 
-$(function(){
-	var sun = document.querySelector(".sun");
-	var navi = document.querySelector(".navi");
-	
-	// 모드 변경 이벤트 리스너 등록
-	$("#mode").on("click", toggleMode)
-});
-
 // 모드 변경 시 호출되는 함수
 function toggleMode() {
+	console.log("test");
+    var sun = document.querySelector(".sun"); //아래 원 
+    var navi = document.querySelector(".navi"); //위 메뉴
     let body = document.body;
-    // console.log("test")
-    // <body>에 'light' 클래스 토글하기
-    body.classList.toggle("light");
     
+    body.classList.toggle("light"); // <body>에 'light' 클래스 토글하기
+
     // 아이콘 토글하기
-    let darkIcon = document.querySelector(".fa-regular.fa-lightbulb");
-    let lightIcon = document.querySelector(".fa-solid.fa-lightbulb");
-
+    let darkIcon = document.querySelector(".fa-regular.fa-lightbulb"); //none 으로 숨겨진 아이콘
+    let lightIcon = document.querySelector(".fa-solid.fa-lightbulb"); 
+    
+    // 아이콘 토글하기 , <body>에 'light' 클래스가 있을 때 색상 변경하기 
     if (body.classList.contains("light")) {
-        darkIcon.style.display = "none"; // 어두운 모드 아이콘 숨기기
-        lightIcon.style.display = "inline-block"; // 밝은 모드 아이콘 표시
-    } else {
-        darkIcon.style.display = "inline-block"; // 어두운 모드 아이콘 표시
-        lightIcon.style.display = "none"; // 밝은 모드 아이콘 숨기기
-    }
-
-    // <body>에 'light' 클래스가 있을 때 색상을 화이트로 변경
-    if (body.classList.contains("light")) {
-        document.documentElement.style.setProperty('--color-black', 'black');
+        // 아이콘 숨기기
+        darkIcon.style.display = "none"; 
+        lightIcon.style.display = "inline-block"; 
+        //색상 변경 
+        navi.style.backgroundColor = "var(--bg-dark)";
         sun.style.backgroundColor = "var(--bg-dark)";
-    	navi.style.backgroundColor = "var(--bg-dark)";
+        sun.style.boxShadow = `5px -5px 10px var(--bg-dark)`;
     } else {
-        document.documentElement.style.setProperty('--color-black', 'white');
+        // 아이콘 변경 
+        darkIcon.style.display = "inline-block"; 
+        lightIcon.style.display = "none"; 
+        //색상 변경 
         navi.style.backgroundColor = "var(--bg-nav)";
-    	sun.style.backgroundColor = "var(--bg-nav)";
+        sun.style.backgroundColor = "var(--bg-nav)";
+        sun.style.boxShadow = `5px -5px 10px var(--bg-light)`;
     }
 }
+let mode = document.getElementById("mode"); //버튼
+console.log(mode);
+mode.addEventListener("click", toggleMode); // 모드 변경 이벤트 리스너 등록
+
+
+
+
