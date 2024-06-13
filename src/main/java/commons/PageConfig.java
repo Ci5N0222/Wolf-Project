6d574facd8be230d0d79828c1ce6d51f8f9f037c
yangharam <haram0704@naver.com>
2024-06-13 09:21:33 +0900

    modified

--- src/main/java/commons/PageConfig.java
+++ src/main/java/commons/PageConfig.java
@@ -1,6 +1,6 @@
 package commons;
 
-public class BoardConfig {
+public class PageConfig {
 	//3 page Navigator 를 몇개씩 보여줄것인지 결정
 		public static int naviCountPerPage=10;
 		public static int recordCountPerPage=10;
