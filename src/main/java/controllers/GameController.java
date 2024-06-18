4752294d8e0ff914c31de9af209e90f46135c356
ol7mi <dmsal9865@gmail.com>
2024-06-18 15:22:20 +0900

    gameDetail.jsp 이미지

--- src/main/java/controllers/GameController.java
+++ src/main/java/controllers/GameController.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.jasper.tagplugins.jstl.core.ForTokens;
+
 import com.google.gson.Gson;
 
 import game.dao.GameDAO;
