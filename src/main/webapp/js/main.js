9cdb7704a85bdf593c3e3501060ad52d760de037
ol7mi <dmsal9865@gmail.com>
2024-06-24 17:28:11 +0900

    게임 페이지 모바일 화면 수정, main.js 토글 에러 수정

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,59 +1,42 @@
 // 모드 변경 시 호출되는 함수
 function toggleMode() {
-    let navi = document.querySelector(".navi"); // 메뉴
-    let mobileNavi = document.querySelector(".m_navi"); // 메뉴
-    let sun = document.querySelector(".sun"); // 달
     let body = document.body;
+    body.classList.toggle("light");
   
-    body.classList.toggle("light"); // <body>에 'light' 클래스 토글
-  
-    // 아이콘 토글
-    let darkIcon = document.querySelector(".fa-regular.fa-lightbulb"); // none 으로 숨겨진 아이콘
-    let lightIcon = document.querySelector(".fa-solid.fa-lightbulb");
-  
-    // 아이콘 토글 <body>에 'light' 클래스가 있을 때 색상 변경
-    if (body.classList.contains("light")) {
-      // 아이콘 변경
-      darkIcon.style.display = "none";
-      lightIcon.style.display = "inline-block";
-      // 색상 변경
-      navi.style.backgroundColor = "var(--lightNav)";
-      mobileNavi.style.backgroundColor = "var(--lightNav)";
-      sun.style.background = "var(--lightMode)";
-      sun.style.boxShadow = ` 0px 5px 20px #999`;
-    } else {
-      // 아이콘 변경
-      darkIcon.style.display = "inline-block";
-      lightIcon.style.display = "none";
-      // 색상 변경
-      navi.style.backgroundColor = "var(--darkNav)";
-      mobileNavi.style.backgroundColor = "var(--darkNav)";
-      sun.style.background = "var(--darkMode)";
-      sun.style.boxShadow = ` 0px 5px 20px #444`;
-    }
+    // 현재 모드 상태를 로컬 스토리지에 저장
+    localStorage.setItem("mode", body.classList.contains("light") ? "light" : "dark");
 
-  }
+    // 아이콘 토글
+    document.querySelector(".fa-regular.fa-lightbulb").style.display = body.classList.contains("light") ? "none" : "inline-block";
+    document.querySelector(".fa-solid.fa-lightbulb").style.display = body.classList.contains("light") ? "inline-block" : "none";
+
+    // 색상 변경
+    let navColor = body.classList.contains("light") ? "var(--lightNav)" : "var(--darkNav)";
+    let sunColor = body.classList.contains("light") ? "var(--lightMode)" : "var(--darkMode)";
+    let boxShadow = body.classList.contains("light") ? "0px 5px 20px #999" : "0px 5px 20px #444";
+
+    document.querySelector(".navi").style.backgroundColor = navColor;
+    document.querySelector(".m_navi").style.backgroundColor = navColor;
+    let sun = document.querySelector(".sun");
+    sun.style.background = sunColor;
+    sun.style.boxShadow = boxShadow;
+}
+
+// 페이지 로드 시 모드 상태 복원
+document.addEventListener("DOMContentLoaded", function () {
+    // 저장된 모드 상태 확인 및 설정
+    if (localStorage.getItem("mode") === "light") {
+        toggleMode();
+    }
   
-  // 페이지 로드 시 모드 상태 복원
-  document.addEventListener("DOMContentLoaded", function () {
-      // 저장된 모드 상태 확인
-      var mode = localStorage.getItem("mode");
-      console.log("test: " + mode);
-      if (mode === "light") {
-          toggleMode(); // light 모드인 경우 토글 함수 호출하여 변경
-      }
-  
-      // 현재 페이지 주소
-      var pathname = window.location.pathname;
-      if (pathname === "/" || pathname === "/index.jsp") homeBinding();
-  
-  
-  });
-  
-  // 모드 변경 이벤트 리스너 등록
-  let mode = document.getElementById("mode"); //버튼
-  mode.addEventListener("click", toggleMode);
-  
+    // 현재 페이지 주소
+    var pathname = window.location.pathname;
+    if (pathname === "/" || pathname === "/index.jsp") homeBinding();
+});
+
+// 모드 변경 이벤트 리스너 등록
+document.getElementById("mode").addEventListener("click", toggleMode);
+
   
   /** Home 입장 시 게임 데이터 받아서 카드에 바인딩 **/
   const homeBinding = () => {
